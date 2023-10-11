837e8447fbf5f6c159bb49042ea2e57dff3e89ed
ASweet10 <asweet1234@yahoo.com>
2023-10-11 01:19:18 -0500

    10.11.23 commit 2
    Added JS Todo list & react templates

---
+++ JS__ToDoList/README.md
@@ -0,0 +1,5 @@
+# JavaScript To-Do List
+## -Tutorial: https://www.makeuseof.com/how-to-build-todo-list-app-using-javascript/
+# Goals
+## -Learn basics of JS
+## -Add extra functionality
---
+++ React__Templates+Pages/src/hooks/useWorkoutsContext.js
@@ -0,0 +1,15 @@
+/*
+import { WorkoutsContext } from '../context/WorkoutsContext'
+import { useContext } from 'react'
+
+export const useWorkoutsContext = () => {
+    const context = useContext(WorkoutsContext)
+
+    //Checks to see if context is used within scope of WorkoutContextProvider in Index.js
+    if(!context) {
+        throw Error('useWorkoutsContext must be used inside a WorkoutsContextProvider')
+    }
+
+    return context
+}
+*/
